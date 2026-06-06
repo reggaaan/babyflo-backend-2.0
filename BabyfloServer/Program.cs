bf2c12d83f0124b64fe858d65beb9e04e8942a9d
reggaaan <reggaan1@gmail.com>
2026-06-06 16:27:53 +0800

    Update Program.cs

--- BabyfloServer/Program.cs
+++ BabyfloServer/Program.cs
@@ -11,7 +11,33 @@ builder.Services.AddCors(options =>
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
+using BabyfloServer.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Define the CORS Policy EXACTLY like this
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite("Data Source=babyflo.db"));
+
+var app = builder.Build();
+
+// 2. CRITICAL: UseCors MUST come BEFORE MapControllers
+app.UseRouting(); 
+app.UseCors("AllowAll"); 
+
+app.MapControllers();
+
+app.Run();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite("Data Source=babyflo.db"));
@@ -20,5 +46,11 @@ var app = builder.Build();
 
 app.UseCors("AllowAll"); // Enable CORS here
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.EnsureCreated(); // Automatically creates tables if missing
+}
 
 app.Run();
