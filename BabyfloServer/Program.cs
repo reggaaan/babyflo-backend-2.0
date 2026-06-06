7a88a5cac2457f2ac8811c723598a8609a5ec863
reggaaan <reggaan1@gmail.com>
2026-06-06 16:29:10 +0800

    Update Program.cs

--- BabyfloServer/Program.cs
+++ BabyfloServer/Program.cs
@@ -11,19 +11,6 @@ builder.Services.AddCors(options =>
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
-using BabyfloServer.Data;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// 1. Define the CORS Policy EXACTLY like this
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy => policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-});
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
@@ -31,26 +18,26 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 var app = builder.Build();
 
-// 2. CRITICAL: UseCors MUST come BEFORE MapControllers
-app.UseRouting(); 
+// --- ADD THIS NEW CODE HERE TO CREATE DATABASE TABLES ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DataContext>();
+        context.Database.EnsureCreated(); // Creates the .db file and tables automatically
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+// --- END OF NEW CODE ---
+
+app.UseRouting();
 app.UseCors("AllowAll"); 
 
 app.MapControllers();
-
-app.Run();
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlite("Data Source=babyflo.db"));
-
-var app = builder.Build();
-
-app.UseCors("AllowAll"); // Enable CORS here
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
-    db.Database.EnsureCreated(); // Automatically creates tables if missing
-}
 
 app.Run();
